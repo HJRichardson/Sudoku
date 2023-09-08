547d63d8af9985218ade7a6a7ab418723dd9a252
Swurve <hj1.richardson@gmail.com>
2023-09-08 14:24:25 +0100

    Added more javadoc to functions

--- src/XGameGrid.java
+++ src/XGameGrid.java
@@ -1,17 +1,41 @@
 public class XGameGrid extends GameGrid {
     
+    /**
+     * Constructor to create an XGameGrid instance from
+     * a 2D array containing the sudoku values.
+     * @param grid - The grid to be initialised.
+     */
     public XGameGrid(int[][] grid) {
         super(grid);
     }
 
+    /**
+     * Constructor to create an XGameGrid instance from
+     * a file path.
+     * @param path - The file path to the sudoku file.
+     */    
     public XGameGrid(String path) {
         super(path);
     }
 
+    /**
+     * Constructor to create an XGameGrid instance from
+     * another GameGrid instance.
+     * @param gameGrid - The GameGrid instance.
+     */    
     public XGameGrid(GameGrid gameGrid) {
         super(gameGrid);
     }
 
+    /**
+     * Checks whether the value could be placed in the diagonal(s).
+     * 
+     * @param row - Row of the grid.
+     * @param col - Column of the grid.
+     * @param value - Value to set.
+     * @param explanation - Whether the program should display why an input cannot be made.
+     * @return If the value is valid in terms of the diagonal(s).
+     */
     private boolean checkDiagonal(int row, int col, int value, boolean explanation) {
         if (row != col && row != GameGrid.GRID_DIM - col - 1) {
             return false;
@@ -39,39 +63,27 @@ public class XGameGrid extends GameGrid {
         return false;
     }
 
-    private boolean checkDiagonalSolver(int row, int col, int value) {
-        if (row != col && row != GameGrid.GRID_DIM - col - 1) {
-            return false;
-        }
-
-        if (row == col) {
-            for (int i = 0; i < GameGrid.GRID_DIM; i++) {
-                if (row != i && grid[i][i].getValue() == value) {
-                    return true;
-                }
-            }
-        } else {
-            for (int i = 0; i < GameGrid.GRID_DIM; i++) {
-                if (row != i && grid[i][GRID_DIM-i-1].getValue() == value) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
+    /**
+     * Checks whether the value to set is a valid move, using the original rules
+     * and the new diagonal rule.
+     * 
+     * @param row - Row of the grid.
+     * @param col - Column of the grid.
+     * @param value - Value to set.
+     * @param explanation - Whether the program should display why an input cannot be made.
+     * @return If the value is a valid move.
+     */
     @Override
     protected boolean isValid(int row, int col, int value, boolean explanation) {
         boolean checkOriginalRules = super.isValid(row, col, value, explanation);
         return checkOriginalRules && !checkDiagonal(row, col, value, explanation);
     }
 
-    @Override
-    protected boolean isValidSolver(int row, int col, int value) {
-        boolean checkOriginalRules = super.isValidSolver(row, col, value);
-        return checkOriginalRules && !checkDiagonalSolver(row, col, value);
-    }
-
+    /**
+     * Returns a string representation of the grid, with the diagonal entries
+     * surrounded by braces to make them stand out..
+     * @return A string representation of the grid.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
